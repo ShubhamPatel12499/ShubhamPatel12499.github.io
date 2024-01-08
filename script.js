5f0ee25c7319f3fda0710a1fce0310f45968e9dc
ShubhamPatel12499 <shubhampatel12499@gmail.com>
2024-01-08 17:31:50 +0530

    Updated Portfolio

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Full-Stack Web Developer", "Designer"],
+    strings: ["Full-Stack Web Developer", "Coder", "Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Full-Stack Web Developer", "Designer"],
+    strings: ["Full-Stack Web Developer", "Coder", "Designer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
